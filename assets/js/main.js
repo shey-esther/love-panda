cf3015ada68dbe5b3d306e00ef7f07f09dbe4847
shey-esther <estheralvaro44@gmail.com>
2017-08-09 10:26:24 -0700

    panditas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
+//insertando imagen desde js.
+var imagen = document.getElementById('imagen');
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
-var img1 = document.createElement('img');
-var img2 = document.createElement('img');
-var img3 = document.createElement('img');
-var img4 = document.createElement('img');
 
-imagen.src = ('assets/img/a1.jpg');
-imagen.src = ('assets/img/a1.jpg');
-imagen.src = ('assets/img/a1.jpg');
-imagen.src = ('assets/img/a1.jpg');
+for (var i = 0; i < pictures.length; i++) {
+	pictures[i];
+	imagen.setAttribute('class', 'img');
+	var img = document.createElement('img');
+	img.src =pictures[i];
+	img.setAttribute('class','panda')
+	imagen.appendChild(img);
+}
