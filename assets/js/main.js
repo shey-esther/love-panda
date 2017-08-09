9c13c9f077af32fa6ace1c99f34df80af187d40f
shey-esther <estheralvaro44@gmail.com>
2017-08-09 01:47:14 -0700

    insertado imagenes fondos y textos

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+var img1 = document.createElement('img');
+var img2 = document.createElement('img');
+var img3 = document.createElement('img');
+var img4 = document.createElement('img');
+
+imagen.src = ('assets/img/a1.jpg');
+imagen.src = ('assets/img/a1.jpg');
+imagen.src = ('assets/img/a1.jpg');
+imagen.src = ('assets/img/a1.jpg');
